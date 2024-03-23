98e90782d985b3ea9c4e042a293ef56efa3db31a
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2024-03-23 20:49:22 -0300

    fix: core folder - components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component'; // Ajuste o caminho conforme a sua estrutura
-import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component'; // Ajuste o caminho conforme a sua estrutura
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [
