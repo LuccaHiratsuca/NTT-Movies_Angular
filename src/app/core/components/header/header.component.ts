e632363df2e4163e2e85cd9bfc9a58fbb70b5bfe
Lucca Hiratsuca Costa <luccahiratsuca@gmail.com>
2024-03-24 20:20:47 -0300

    feat: header component

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -10,7 +12,20 @@ export class HeaderComponent {
   searchText: string = ''; 
   showSearch: boolean = false;
 
+  constructor(private router: Router) {
+  }
+
   toggleSearch(): void {
     this.showSearch = !this.showSearch;
   }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        console.log('Navigation started to:', event.url);
+      }
+    });
+  }
+
+    
 }
